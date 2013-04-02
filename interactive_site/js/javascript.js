3ba69a0065c4050fc00a163ccf27990bc382c63a
krhogan <krhogan103@yahoo.com>
2013-04-01 23:13:50 -0400

    icons linked to recipes
    after fruitsalad not working!

--- interactive_site/js/javascript.js
+++ interactive_site/js/javascript.js
@@ -48,6 +48,54 @@ $(document).ready(function() {
      $("#recipe-burrito").show();
 	});
 	
+	$("#sandwich > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("#recipe-sandwich").show();
+	});
+	
+	$("#salad > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("#recipe-salad").show();
+	});
+	
+	$("#fruitsalad > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("recipe-fruitsalad").show();
+	});
+	
+	$("#wrap > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("recipe-wrap").show();
+	});
+	
+	$("#chickenkiwi > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("recipe-chickenkiwi").show();
+	});
+	
+	$("#pizza > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("recipe-pizza").show();
+	});
+	
+	$("#orangechicken > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("recipe-orangechicken").show();
+	});
+	
+	$("#berrychops > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("recipe-berrychops").show();
+	});
+	
 	$(".ingredients > h3").on("click",function(){
 		$(".ingredients-content").hide()
 		$(".ingredients-content > ul").show();
