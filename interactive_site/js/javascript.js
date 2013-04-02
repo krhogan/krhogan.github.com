07b7a47aa5dfcd77294117656a0882ae86627031
krhogan <krhogan103@yahoo.com>
2013-04-01 22:03:24 -0400

    second and third page

--- interactive_site/js/javascript.js
+++ interactive_site/js/javascript.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
-
-	$("#crisp > img").on("click",function(){
-   		$(".recipes > div").hide()
-    	$("#recipe-crisp").show();
+	
+	$("#second-page").hide();
+	$("#third-page").hide();
+	
+	 $("#breakfast-selector > img").on("click",function(){
+   	 $("#second-page").show(); 
+     $(".course > div").hide()
+     $("#breakfast").show();
 	});
 	
-	$("#breakfast-selector > img").on("click",function(){
-		$(".course > div").hide()
-		$("breakfast").show();
+	$("#lunch-selector > img").on("click",function(){
+   	 $("#second-page").show(); 
+     $(".course > div").hide()
+     $("#lunch").show();
+	});
+	
+	$("#dinner-selector > img").on("click",function(){
+   	 $("#second-page").show(); 
+     $(".course > div").hide()
+     $("#dinner").show();
+	});
+	
+	$("#crisp > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("#recipe-crisp").show();
+	});
+	
+	$("#pancake > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("#recipe-pancake").show();
+	});
+	
+	$("#parfait > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("#recipe-parfait").show();
+	});
+	
+	$("#burrito > img").on("click",function(){
+   	 $("#third-page").show(); 
+     $(".recipes > div").hide()
+     $("#recipe-burrito").show();
 	});
 	
 	$(".ingredients > h3").on("click",function(){
@@ -16,3 +51,6 @@ $(document).ready(function() {
 	});
 
  });
+ 
+ 
+ 
